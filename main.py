7d59b89d6e9127c9f918a48ad6a99fab671e9702
juntaek-lim-div <juntaek.lim@dongiltech.co.kr>
2024-01-19 10:56:08 +0900

    .

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import a
 import b
+import os
 
+RootDIR = os.path.dirname(os.path.abspath(__file__))
 
+#read test.txt
+with open(RootDIR + '/test.txt', 'r') as f:
+    print(f.read())
 for i in range(10):
     a.add(i,i)
     b.sub(i,i)
+os.system('pause')
