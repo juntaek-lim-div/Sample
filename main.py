ce3507250f03dabfe0301c44ca47072387569bc2
juntaek-lim-div <juntaek.lim@dongiltech.co.kr>
2024-01-22 11:50:36 +0900

    dll test

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import a
 import b
 import os
+import ctypes
 
 RootDIR = os.path.dirname(os.path.abspath(__file__))
-
+print(f"RootDIR: {RootDIR}")
 #read test.txt
 with open(RootDIR + '/test.txt', 'r') as f:
     print(f.read())
+    
+#load dll
+dll = ctypes.cdll.LoadLibrary(RootDIR + '/opu.dll')
 for i in range(10):
     a.add(i,i)
     b.sub(i,i)
