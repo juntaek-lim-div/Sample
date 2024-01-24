26cd2eba86d9a4943c2e640f68d8906c1e06ae02
juntaek-lim-div <juntaek.lim@dongiltech.co.kr>
2024-01-24 09:19:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ import ctypes
 
 
 if(__name__=="__main__"):
-    print("asdf")
+    print("program is running...")
+    for i in range(0, 100):
+        print("i = " + str(i))
+        a.add(i,i)
+        b.sub(i,i)
     os.system("pause")
