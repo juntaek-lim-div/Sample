29667ba5fe712055b4efc9b63e82751bf1bae080
juntaek-lim-div <juntaek.lim@dongiltech.co.kr>
2024-01-24 20:18:47 +0900

    add ##check##

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import os
 import ctypes
 
 
+
+##check##
+
+
 if(__name__=="__main__"):
     print("program is running...")
     for i in range(0, 100):
         print("i = " + str(i))
         a.add(i,i)
+        ##check##
         b.sub(i,i)
     os.system("pause")
